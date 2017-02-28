f18837676d4049f7bc89ec95d7e6247abba5350b
Billy Liggins <Billyliggins@gmail.com>
2017-02-28 16:09:23 -0500

    Moved PasswordDialog into the main app.py

--- pollingGui/src/App.py
+++ pollingGui/src/App.py
@@ -2,10 +2,10 @@
 #import matplotlib
 #matplotlib.use("TkAgg")
 #
-import math
+import os
+import numpy as np
 #from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 #from matplotlib.figure import Figure
-import random
 import Tkinter
 import Tkinter as tk
 import ttk
@@ -15,29 +15,65 @@ from snotdaq.datastream import *
 import time
 import socket
 import struct
-import numpy as np
-# import demo_tooltips_2
-import os
 import time 
-#from Tkinter import messagebox
 import tkMessageBox
 import webbrowser
-#fix for monug2
-from datastream import parse_cmos_record
+##fix for monug2
+#from datastream import parse_cmos_record
 
 
 
 # Imports for PSQL access
 import psycopg2
 import psycopg2.extras
-import passwordDialog
 
-#------------------------------------
-# You need to make a cell manger that makes the cells and then handles the updateing.
-#
-#
-#
-#------------------------------------
+
+class PasswordDialog(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        #top = self.top = tk.Toplevel(parent)
+        tk.Label(self, text="database host").grid(row=0,column=0, sticky=tk.W,padx=10)
+        self.a = tk.Entry(self)
+        self.a.insert(0,'192.168.80.120')
+        # self.a.insert(0,'pgsql.snopl.us')
+        self.a.grid(row=0,column=1, sticky=tk.W)
+
+        tk.Label(self, text="database name").grid(row=1,column=0, sticky=tk.W,padx=10)
+        self.b = tk.Entry(self)
+        self.b.insert(0,'detector')
+        self.b.grid(row=1,column=1, sticky=tk.W)
+
+        tk.Label(self, text="database user").grid(row=2,column=0, sticky=tk.W,padx=10)
+        self.c = tk.Entry(self)
+        # self.c.insert(0,'alarm_gui')
+        self.c.insert(0,'snoplus')
+        self.c.grid(row=2,column=1, sticky=tk.W)
+
+        tk.Label(self, text="database password").grid(row=3,column=0, sticky=tk.W,padx=10)
+        self.d = tk.Entry(self,show='x')
+        self.d.insert(0,'')
+        self.d.focus_set()
+        self.d.bind('<Return>', self.check)
+        self.d.grid(row=3,column=1, sticky=tk.W)
+
+        b = tk.Button(self, text="Submit", command=self.check)
+        b.grid(row=4,columnspan=2)
+
+
+    def check(self, *args):
+        self.host = self.a.get()
+        self.name = self.b.get()
+        self.user = self.c.get()
+        self.password = self.d.get()
+        try:
+            # Try establishing connection. This is the sure way to know if the
+            # password is correct.
+            conn = psycopg2.connect('dbname=%s user=%s password=%s host=%s connect_timeout=5'
+                                    % (self.name, self.user, self.password, self.host))
+            conn.close()
+            self.destroy()
+        except psycopg2.Error as e:
+            tkMessageBox.showerror('Error',('Unable to connect! {0} \nMight be due to no connection to the DB or a wrong password').format(e))
 
 class rect():
    
@@ -269,7 +305,7 @@ class App():
             #                       % ("detector", "snoplus","192.168.80.120" ))
             #self.conn = psycopg2.connect()
         except Exception as e:
-            self.d =  passwordDialog.PasswordDialog(self.master)
+            self.d = PasswordDialog(self.master)
             self.d.pack()
             self.master.wait_window(self.d)
 
@@ -733,7 +769,7 @@ class App():
         """This function rounds and millifies a number, mainly for CMOS rates"""
         n = float(n)
         millidx = max(0,min(len(self.millnames)-1,
-                            int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+                            int(np.floor(0 if n == 0 else np.log10(abs(n))/3))))
 
         return ["%.1f"%(n / 10**(3 * millidx)), self.millnames[millidx]]
 

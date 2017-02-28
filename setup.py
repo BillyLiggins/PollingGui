d50f4a8c4a44afcfc8eea1e869681344105c182d
Billy Liggins <Billyliggins@gmail.com>
2017-02-28 16:08:27 -0500

    First attempt at adding the python setup.

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+""" Setup the setup """
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+pollingGui
+------------
+GUI for displaying pooled base currents and CMOS rate in SNO+
+"""
+
+setup(
+    name="pollingGui",
+    version="0.1",
+    author="Billy Liggins",
+    author_email="billy.liggins@qmul.ac.uk",
+    url="https://github.com/BillyLiggins/PollingGui",
+    description="SNO+ polling GUI",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    platforms=["any"],
+    packages=['pollingGui'],
+    package_data={'': ['*.md']},
+    install_requires=['pyttk>=0.3.1', 'psycopg2>=2.0.14'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pollinggui=pollingGui:main',
+        ]
+    }
+)
